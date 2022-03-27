9f0cce9006c9105051c1d7b60e606d3ea38c07ad
Ramata <ramataka81@gmail.com>
2022-03-27 19:48:55 +0000

    ajout Pays dans mon projet laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PaysController;
+use App\Models\Pays;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $pays = Pays::all();
+    return view('welcome', [
+        'pays' => $pays
+    ]);
 });
+
+
+Route::get('/pays',[PaysController::class, 'index']);
